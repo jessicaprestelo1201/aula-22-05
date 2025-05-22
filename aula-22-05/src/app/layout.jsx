511cd220123b29c2aa529aff23b5446063b00ada
jessicaprestelo1201 <jessica.p.jesus@aluno.senai.br>
2025-05-22 10:52:56 -0300

    feat: finalização

--- aula-22-05/src/app/layout.jsx
+++ aula-22-05/src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "./context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,14 +14,14 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Autenticação com Next.js",
-  description: "Projeo de autenticação com Next.js utilizando API externa",
+  description: "Projeto de autenticação com Next.js utilizando API externa",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
